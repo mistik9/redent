365f3fdc1cc74dfa7fd7c5c4f1bc76dae152d91e
mistik9 <mistik9@icloud.com>
2023-11-21 15:19:53 +0600

    add easter teeth

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -5,60 +5,78 @@ import casts from "../../image/icon5.png";
 import imp from "../../image/icon7.png";
 import other from "../../image/icon.png";
 import pulpitis from "../../image/icon1.png";
-import { price_therapy, price_pulpitis, price_surgury, price_casts, price_implant, price_other } from "../../utils/const";
-
+import {
+  price_therapy,
+  price_pulpitis,
+  price_surgury,
+  price_casts,
+  price_implant,
+  price_other,
+} from "../../utils/const";
+import React from "react";
 import "./Price.css";
 
 function Price() {
+  const [easter, setEaster] = React.useState(therapy);
+
+  function onTherapyClick() {
+    setEaster(clean);
+  }
+
   return (
     <section className="priсe content__section " id="price">
       <h2 className="section__title">Стоимость услуг</h2>
       <ul className="price__list">
-        <img className="price__img" src={therapy} alt="лечение" />
-        {price_therapy.map(i =>
+        <img
+          className="price__img"
+          src={easter}
+          alt="лечение"
+          onClick={onTherapyClick}
+        />
+        {price_therapy.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
-        <img className="price__img" src={pulpitis} alt="лечение"></img>
-        {price_pulpitis.map(i =>
+        ))}
+        <img className="price__img" src={pulpitis} alt="пульпит"></img>
+        {price_pulpitis.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
+        ))}
         <img className="price__img" src={surg} alt="хирургия"></img>
-        {price_surgury.map(i =>
+        {price_surgury.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
+        ))}
         <img className="price__img" src={casts} alt="протезирование"></img>
-        {price_casts.map(i =>
+        {price_casts.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
+        ))}
         <img className="price__img" src={imp} alt="импланты"></img>
-        {price_implant.map(i =>
+        {price_implant.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
+        ))}
         <img className="price__img" src={other} alt="другое"></img>
-        {price_other.map(i =>
+        {price_other.map((i) => (
           <li className="price__list-item" key={i.name}>
             <p calss="price__name">{i.name}</p>
             <p className="price__value"> {i.cost}</p>
           </li>
-        )}
+        ))}
       </ul>
     </section>
-  )
+  );
 }
 
-export default Price
+export default Price;
